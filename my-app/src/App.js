7645b1fcc5d740325d2b4c90e8ae62bde2d6ee22
Andrey Lashkevich <andlashkevich@ya.ru>
2024-11-28 17:35:37 +0300

    2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [reppass, setReppass] = useState('');
+	const [passwordError, setPasswordError] = useState(null);
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		console.log({ email, password, reppass });
+	};
+	const onPassChange = ({ target }) => {
+		setPassword(target.value);
+		let error = null;
+		if (!/^[\p{Alpha}\p{Nd}_]*$/.test(target.value)) {
+			error =
+				'Недопустимые символы. Пароль может содержать только буквы, цифры, нижнее подчеркивание.';
+		} else if (target.value.length > 15) {
+			error = 'Допустимо не более 15 символов';
+		}
+		setPasswordError(error);
+	};
+	const onBlur = () => {
+		if (password.length < 3) setPasswordError('Допустимо не менее 3 символов');
+	};
+
+	return (
+		<>
+			<form className={styles.login} onSubmit={onSubmit}>
+				<h1>Пройдите регистрацию </h1>
+				{passwordError && <div className={styles.error}>{passwordError}</div>}
+				<input
+					type="email"
+					name="email"
+					value={email}
+					onChange={({ target }) => setEmail(target.value)}
+					className={styles['login-input']}
+					placeholder="Введите email"
+					autoFocus="on"
+					required={true}
+					autoComplete="on"
+				/>
+				<input
+					type="password"
+					name="password"
+					value={password}
+					onChange={onPassChange}
+					onBlur={onBlur}
+					className={styles['login-input']}
+					placeholder="Введите пароль"
+					autoComplete="on"
+				/>
+				<input
+					type="password"
+					name="reppass"
+					value={reppass}
+					onChange={({ target }) => setReppass(target.value)}
+					className={styles['login-input']}
+					placeholder="Повторите пароль"
+					autoComplete="on"
+				/>
+				<button
+					type="submit"
+					className={styles['login-submit']}
+					disabled={!!passwordError}
+					// style={{ display: !!passwordError ? 'none' : 'block' }}
+				>
+					Зарегистрироваться
+				</button>
+			</form>
+		</>
+	);
 }
 
 export default App;
