aa2b690e9c8db0600dfe38d6a17ff47885262c72
Andrey Lashkevich <andlashkevich@ya.ru>
2024-11-29 21:04:40 +0300

    3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,82 @@
 import styles from './App.module.css';
 import { useState } from 'react';
+import { useRef } from 'react';
 
 function App() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-	const [reppass, setReppass] = useState('');
-	const [passwordError, setPasswordError] = useState(null);
+	const [repass, setRepass] = useState('');
+	const [errorM, setErrorM] = useState(null);
+	const myRef = useRef(null);
+	const subOff = !email || !password || !repass || errorM;
+	const valid = {
+		emailRegexp: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+		passRegexp: /^[\p{N}\p{L}_]{0,20}$/gmu,
+	};
+	const mes = {
+		badpass:
+			'Недопустимый ввод. Пароль может содержать любые буквы и цифры и только. Не менеее 6 и не более 20 знаков.',
+		badmail: 'Неприемлемый адрес электронной почты',
+		small: 'Пароль слишком короткий. Менее 6 символов',
+		diff: 'Пароли должны совпадать',
+	};
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		console.log({ email, password, reppass });
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(
+			'email-',
+			email,
+			'\n',
+			'password_1-',
+			password,
+			'\n',
+			'password_2-',
+			repass,
+			errorM,
+		);
+		setEmail('');
+		setPassword('');
+		setRepass('');
 	};
+	const emailBlur = () => {
+		// let error = null;
+		if (!valid.emailRegexp.test(email)) setErrorM(mes.badmail);
+		// setErrorM(error);
+	};
+
 	const onPassChange = ({ target }) => {
-		setPassword(target.value);
 		let error = null;
-		if (!/^[\p{Alpha}\p{Nd}_]*$/.test(target.value)) {
-			error =
-				'Недопустимые символы. Пароль может содержать только буквы, цифры, нижнее подчеркивание.';
-		} else if (target.value.length > 15) {
-			error = 'Допустимо не более 15 символов';
-		}
-		setPasswordError(error);
+		// !valid.emailRegexp.test(email)
+		// 	? (error = mes.badmail)
+		// : // setErrorM(mes.badmail)
+		setPassword(target.value);
+		if (password && !valid.passRegexp.test(target.value)) error = mes.badpass;
+		// setErrorM(mes.badpass);
+		setErrorM(error);
 	};
-	const onBlur = () => {
-		if (password.length < 3) setPasswordError('Допустимо не менее 3 символов');
+	const passBlur = () => {
+		// if (password.length < 6) setErrorM(mes.small);
+	};
+
+	const onRepassChange = ({ target }) => {
+		setRepass(target.value);
+		if (!valid.passRegexp.test(target.value)) setErrorM(mes.badpass);
+	};
+	const repassBlur = () => {
+		repass !== password ? setErrorM(mes.diff) : myRef.current.focus();
 	};
 
 	return (
 		<>
 			<form className={styles.login} onSubmit={onSubmit}>
 				<h1>Пройдите регистрацию </h1>
-				{passwordError && <div className={styles.error}>{passwordError}</div>}
+				{errorM && <div className={styles.error}>{errorM}</div>}
 				<input
 					type="email"
 					name="email"
 					value={email}
 					onChange={({ target }) => setEmail(target.value)}
+					onBlur={emailBlur}
 					className={styles['login-input']}
 					placeholder="Введите email"
 					autoFocus="on"
@@ -47,7 +88,7 @@ function App() {
 					name="password"
 					value={password}
 					onChange={onPassChange}
-					onBlur={onBlur}
+					onBlur={passBlur}
 					className={styles['login-input']}
 					placeholder="Введите пароль"
 					autoComplete="on"
@@ -55,8 +96,9 @@ function App() {
 				<input
 					type="password"
 					name="reppass"
-					value={reppass}
-					onChange={({ target }) => setReppass(target.value)}
+					value={repass}
+					onChange={onRepassChange}
+					onBlur={repassBlur}
 					className={styles['login-input']}
 					placeholder="Повторите пароль"
 					autoComplete="on"
@@ -64,7 +106,8 @@ function App() {
 				<button
 					type="submit"
 					className={styles['login-submit']}
-					disabled={!!passwordError}
+					ref={myRef}
+					disabled={subOff}
 					// style={{ display: !!passwordError ? 'none' : 'block' }}
 				>
 					Зарегистрироваться
